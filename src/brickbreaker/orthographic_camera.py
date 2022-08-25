58f9ddb343e52dd3ae6f3e01c808f54f223cf1e7
twje <11610634+twje@users.noreply.github.com>
2022-08-25 11:20:17 +1000

    Add camera movement

--- src/brickbreaker/orthographic_camera.py
+++ src/brickbreaker/orthographic_camera.py
@@ -5,14 +5,29 @@ class OrthographicCamera:
     def __init__(self) -> None:
         self.viewport_width = 0   # world units
         self.viewport_height = 0  # world units
+        self.position = pyrr.Vector3([0.0, 0.0, 0.0])
+        self.direction = pyrr.Vector3([0.0, 0.0, -1.0])
+        self.up = pyrr.Vector3([0.0, 1.0, 0.0])
+        self.near = 0
+        self.far = 100
         self.combined = None
 
-    def update(self) -> None:               
-        self.combined = pyrr.matrix44.create_orthogonal_projection(
+    def set_position(self, x, y):
+        self.position.x = x
+        self.position.y = y
+
+    def update(self) -> None:
+        view = pyrr.matrix44.create_look_at(
+            self.position,
+            self.position + self.direction,
+            self.up
+        )
+        projection = pyrr.matrix44.create_orthogonal_projection(
             -self.viewport_width/2,
             self.viewport_width/2,
             -self.viewport_height/2,
             self.viewport_height/2,
-            -1000,
-            1000
+            self.near,
+            self.far
         )
+        self.combined = pyrr.matrix44.multiply(projection, view)
